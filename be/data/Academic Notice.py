93ce2110cf55e309a25275f2237105ff66bc5bfe
j-yunjeong <jungy_jung@naver.com>
2022-12-06 16:10:48 +0900

    modify all files in data Directory

--- be/data/Academic Notice.py
+++ be/data/Academic Notice.py
@@ -2,65 +2,72 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+
 """This file is about Academic Notice."""
 
-notices = []
-academic ={}
+def academic_notice(num=1):
+    """Returns parsed infomation about Academic Notice
 
-for page in range(1):
-    response = requests.get("http://hannam.ac.kr/kor/guide/guide_02.html?pPageNo="+str(page)+"&pRowCount=30&pPageNo=" +str(page+1))
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    word = soup.select('td')  # Excluding Attachments
+    Args:
+        num (int): number of page for parsing
+        
+    Returns:
+        list : collecion of Dictionary that is Resulf of parsing for Accademic Notice
+    """
+    notices = []
+    academic ={}
+    
+    for page in range(num):
+        response = requests.get("http://hannam.ac.kr/kor/guide/guide_02.html?pPageNo="+str(page)+"&pRowCount=30&pPageNo=" +str(page+1))
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        word = soup.select('td')  # Excluding Attachments
 
 
-# 파싱 (첨부파일 제외)
-    testList = []
-    for i in word:
-        testList.append(re.sub("\t|\n|\r", "", i.text.strip()).lstrip("번호").lstrip("제목").lstrip("첨부파일").lstrip("작성자").lstrip("작성일").lstrip("조회수"))
+        # Parsing (Expect Attachments)
+        testList = []
+        for i in word:
+            testList.append(re.sub("\t|\n|\r", "", i.text.strip()).lstrip("번호").lstrip("제목").lstrip("첨부파일").lstrip("작성자").lstrip("작성일").lstrip("조회수"))
 
-    for i in range(len(testList)):
-        if i % 6 == 0:
+        # Parsing (Attachment)
+        attach = soup.select('.txt-l > a[href]')       # Attachments 
+        
+        for i in attach:
+            href = i.attrs['href']
+            rec = requests.get("http://hannam.ac.kr" + href)
+            html = rec.text
+            soup = BeautifulSoup(html,"html.parser")
+            pdf = soup.select('.add-file-list > ul > li > a[href]')
+
+            fileNameDict = {}
+            fileLinkDict = {}
+            
+            i = 0 
+            while (i < len(pdf)):
+                tmpPdf = "http"+ ((pdf[i].attrs['href']).lstrip("../include/board_down.php?s_filepath="))
+                fileNameDict['file'+ str(i+1)] = tmpPdf[tmpPdf.find('&filename=')+10 : ]
+                fileLinkDict['file'+ str(i+1)] = tmpPdf[ : tmpPdf.find('&filename=')]
+                i += 1
+                
+            
             academic = {
                 'class' : '학사공지',              # Class
-                'number' : testList[i],            # Number
-                'title' : testList[i+1],           # Title
-                'writer' : testList[i+3],          # Writer
-                'dateCreated' : testList[i+4]      # dateCreated
+                'number' : testList[0],            # Number
+                'title' : testList[1],             # Title
+                'writer' : testList[3],            # Writer
+                'dateCreated' : testList[4],       # dateCreated
+                'postLink' : "http://hannam.ac.kr" + href,
+                'fileName' : fileNameDict,
+                'fileLink' : fileLinkDict
             }
-            i += 6
-            notices.append(academic)
-
             
-# Parsing (Attachment)
-    attach = soup.select('.txt-l > a[href]')       # Attachments
-
-    pdfs = {}
-    pdfInfoList = []
-
-    for i in attach:
-        href = i.attrs['href']
-        rec = requests.get("http://hannam.ac.kr" + href)
-        html = rec.text
-        soup = BeautifulSoup(html,"html.parser")
-        
-        
-        noticeTitle = soup.select('.post-title')
-        title = noticeTitle[0].text
-        pdf = soup.select('.add-file-list > ul > li > a[href]')
-        
-        for i in range(len(pdf)):
-        
-            if pdf[i] != []:
-                tmpPdf = "http"+ ((pdf[i].attrs['href']).lstrip("../include/board_down.php?s_filepath="))
-                pdfs = {
-                'fileName' : tmpPdf[tmpPdf.find('&filename=')+10 : ],
-                'fileLink' : tmpPdf[ : tmpPdf.find('&filename=')],
-                'title' :  title 
-                }
-                pdfInfoList.append(pdfs)
+            notices.append(academic)
+            testList = testList[6:]
+            
+            
+    return notices
 
 
-# print result                                 
-print(notices)
-print(pdfInfoList)   
+
+for post in academic_notice(1):
+    print(post)
