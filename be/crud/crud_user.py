fc5bf7dd7b5c74fbb158a4b8e2857608db2e47b2
Choi JaeHoon <jhchoi09@naver.com>
2023-01-04 00:20:33 +0900

    �Success User Login/Logout JWT Authentic Flow

--- be/crud/crud_user.py
+++ be/crud/crud_user.py
@@ -4,35 +4,19 @@ from datetime import date
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from db.database import db
 
-def createAccount(profileImg_link:str, name:str, nickName:str, userId:str, userPw:str, studentCode:int, birth_year:int, birth_month:int, birth_day:int, email:str, keywords:list, recentPost:list):
+def createAccount(profileImg_link:str, name:str, nickName:str, userPw:str, studentCode:int, birth_year:int, birth_month:int, birth_day:int, email:str, keywords:list, recentPost:list):
     newUser = {}
     userBirth = date(birth_year, birth_month, birth_day)
     newUser['profileImg']=profileImg_link
     newUser['name']=name
     newUser['nickName']=nickName
-    newUser['userId']=userId
-    newUser['userPw']=userPw
+    newUser['email']=email
+    newUser['password']=userPw
     newUser['studentCode']=studentCode
     newUser['birth']=userBirth.isoformat()
-    newUser['email']=email
     newUser['keywords']=keywords
     newUser['recentPost']=recentPost
     db["user"].insert_one(newUser)
 
 def createSuperAccount():
     createAccount("dummy profileImgLink", "super", "Administrator", "super", "1234", "10000000", 2023, 1, 1, "aaaa@aaaa", [], [])
-
-# def readAccount():
-#     pass
-
-# def readAccount_all():
-#     pass
-
-# def updateAccount():
-#     pass
-
-# def deleteAccount():
-#     pass
-
-# def deleteAccount_all():
-#     pass
